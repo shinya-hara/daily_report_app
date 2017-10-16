16661ee44e9496be9d9f4ebaeafe84ee9cbffe37
Shinya Hara <s17g477@stu.kagawa-u.ac.jp>
2017-10-17 01:32:39 +0900

    Install config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # devise setup
+  config.action_mailer.default_url_options = { host: 'https://warm-basin-85336.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp.gmail.com',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :user_name => Settings.gmail[:user_name],
+  #   :password => Settings.gmail[:password]
+  # }
 end

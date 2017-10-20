4690c04fb6c2d2482a73cf210a9c6dd15886d3ae
Shinya Hara <s17g477@stu.kagawa-u.ac.jp>
2017-10-20 14:26:13 +0900

    各ユーザの情報を表示するページを作成
    /users/info/:username でアクセス可能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :reports
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/info/:username' => 'users#show'
   root to: 'reports#index'
 end

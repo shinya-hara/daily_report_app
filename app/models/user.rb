c39c5eaa080155abdc46301aa74b7ca7d811629d
Shinya Hara <s17g477@stu.kagawa-u.ac.jp>
2017-10-26 02:47:42 +0900

    パスワード入力なしでユーザ情報を編集できるように変更
    DeviseのConrollerを生成し、update_resourceメソッドをオーバーライドした。
    参考サイト：
    http://easyramble.com/user-account-update-without-password-on-devise.html
    https://github.com/plataformatec/devise/wiki/How-To%3a-Allow-users-to-edit-their-account-without-providing-a-password

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,20 @@ class User < ApplicationRecord
     end
   end
 
+  # ユーザ情報をパスワードなしで編集許可
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   validates :username,
     uniqueness: { case_sensitive: :true },
     length: { minimum: 4, maximum: 20 },

dc54f7102fb8441bec89fdf3257bda5dff400c0f
Shinya Hara <s17g477@stu.kagawa-u.ac.jp>
2017-12-30 10:28:14 +0900

    seedファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,6 @@ User.create(
   )
 end
 
-<<<<<<< HEAD
 # グループ
 (1..2).each do |n|
   Group.create(
@@ -51,8 +50,6 @@ User.all.each do |user|
   )
 end
 
-=======
->>>>>>> 103b105bc44b0357e92a0cc86c13598c2a52666c
 # 日報
 (10..11).each do |month|
   (1..30).each do |day|
